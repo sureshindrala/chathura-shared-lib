49e64df35d31bc71c87d440acbc497b7f23ad7d0
i27devopsb5 <i27devops+b5git@gmail.com>
2024-11-13 07:43:46 +0530

    added vars

---
+++ vars/firstPipeline.groovy
@@ -0,0 +1,60 @@
+import com.i27academy.builds.Calculator
+
+def call(Map pipelineParams) {
+    // an instance of the Calculator class 
+    Calculator calculator = new Calculator()
+    pipeline {
+        agent {
+            label 'java-slave'
+        }
+        environment {
+            APPLICATION_NAME = "${pipelineParams.appName}"
+        }
+        stages {
+            stage ('Calculate') {
+                steps {
+                    script {
+                        echo "Calling Calculator Method from src folder"
+                        echo "************ Printing the sum of values ************"
+                        calculator.add(2,3)
+                    }
+                }
+            }
+            stage ('Build') {
+                steps {
+                    echo "Building the project"
+                }
+            }
+            stage ('Test') {
+                steps {
+                    echo "Testing the project"
+                }
+            }
+            stage ('DevDeploy') {
+                steps {
+                    echo "Deploying the project to Dev environment"
+                }
+            }
+            stage ('TestDeploy') {
+                steps {
+                    echo "Deploying the project to Test environment"
+                }
+            }
+            stage ('ProdDeploy') {
+                steps {
+                    echo "Deploying the project to Prod environment"
+                }
+            }
+        }
+    }
+}
+
+def cal(firstNumber, secondNumber) {
+    return firstNumber + secondNumber
+}
+
+// ${variable}
+// ${env.VARIABLE}
+// ${params.VARIABLE}
+// ${pipelineParams.VARIABLE}
+
