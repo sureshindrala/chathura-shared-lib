f87e4bc2613a22436493e70f6ee4e352e9a22ce6
siva <siva@gmail.com>
2025-02-15 20:08:29 +0530

    tried only login

--- src/com/i27academy/k8s/K8s.groovy
+++ src/com/i27academy/k8s/K8s.groovy
@@ -7,12 +7,11 @@ class K8s {
     }
 
     // Method to authenticate to kubernetes clusters
-    def auth_login(clusterName, zone, projectID){
+    def auth_login(){
         jenkins.sh """
             echo "********************* Entering into kubernetes Authentication/login Method *********************"
             gcloud compute instances list
-            echo "********************* Create the config file for the environment *********************"
-            
+            echo "********************* Create the config file for the environment *********************"  
             kubectl get nodes 
         """
     }
