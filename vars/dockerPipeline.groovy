5efdb1e65f925b1852d8417d6abe772fe5659db6
Suresh Indrala <sureshindrala99@gmail.com>
2025-10-01 20:34:21 +0530

    updated ports

--- vars/dockerPipeline.groovy
+++ vars/dockerPipeline.groovy
@@ -40,6 +40,7 @@ def call(Map pipelineParams) {
             TST_HOST_PORT = "${pipelineParams.tstHostPort}"
             STG__HOST_PORT = "${pipelineParams.stgHostPort}"
             PROD__HOST_PORT = "${pipelineParams.prdHostPort}"
+            CONT_PORT = "${pipelineParams.contPort}"
             SONAR_HOST= 'http://34.172.162.27:9000'
             POM_VERSION = readMavenPom().getVersion()
             POM_PACKAGING = readMavenPom().getPackaging()
@@ -261,7 +262,7 @@ def dockerdeploy(envDeploy,envPort) {
 
             // Run new container
             sh """
-            sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no "$USERNAME"@"${env.DOCKER_SERVER}" "docker container run -dit -p ${envPort}:8232 --name ${env.APPLICATION_NAME}-${envDeploy} ${env.DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT}"
+            sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no "$USERNAME"@"${env.DOCKER_SERVER}" "docker container run -dit -p $hostPort:$contPort --name ${env.APPLICATION_NAME}-${envDeploy} ${env.DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT}"
             sshpass -p "$PASSWORD" ssh -o StrictHostKeyChecking=no "$USERNAME"@"${env.DOCKER_SERVER}" "docker ps"
             """
         } catch (err) {
