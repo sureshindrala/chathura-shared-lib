9115250bb61485129b98a8285ae05f0ce2c18178
Suresh Indrala <sureshindrala99@gmail.com>
2025-10-06 10:45:09 +0530

    updated with chathurak8s

---
+++ src/com/chathura/k8s/K8s.groovy
@@ -0,0 +1,28 @@
+package com.chathura.k8s;
+class K8s {
+// write all the methods here 
+    def jenkins
+    K8s(jenkins) {
+        this.jenkins = jenkins
+    }
+
+    // Method to authenticate to kubernetes clusters
+    def auth_login(clusterName, zone, projectID){
+        jenkins.sh """
+            echo "********************* Entering into kubernetes Authentication/login Method *********************"
+            gcloud compute instances list
+            echo "********************* Create the config file for the environment *********************"  
+            gcloud container clusters get-credentials $clusterName --zone $zone --project $projectID
+            kubectl get nodes 
+        """
+    }
+
+    // Method to deploy the application. 
+    def k8sdeploy(fileName, docker_image, namespace){
+        jenkins.sh """
+            echo "********************* Entering into kubernetes Deployment Method *********************"
+            sed -i "s|DIT|${docker_image}|g" ./.cicd/${fileName}
+            kubectl apply -f ./.cicd/${fileName} -n $namespace
+        """
+    }
+}
