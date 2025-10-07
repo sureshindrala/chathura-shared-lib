01ceff3e4aa42d2ea81e9eafae8ae6ff6fcc914f
Suresh Indrala <sureshindrala99@gmail.com>
2025-10-07 19:33:56 +0530

    deleteted docker .cicd

--- vars/nodePipeline.groovy
+++ vars/nodePipeline.groovy
@@ -191,7 +191,7 @@ def dockerBuildandPush() {
     return {
         echo "*****************building Docker image***********************"
         sh """
-            docker build --no-cache  -t ${env.DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT}  ./.cicd
+            docker build --no-cache  -t ${env.DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT} .
             echo "***********Docker login***********************"
             docker login -u ${DOCKER_CREDS_USR} -p ${DOCKER_CREDS_PSW} 
             docker push ${env.DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT}
