e5e83b97c07a4f88bdbfd69365ffae88e99396b5
Suresh Indrala <sureshindrala99@gmail.com>
2026-08-17 18:38:08 +0530

    updated artifact registory image tag

--- vars/google_artifact.groovy
+++ vars/google_artifact.groovy
@@ -280,7 +280,7 @@ def dockerBuildandPush() {
         sh """
             cp ${workspace}/target/chathura-${env.APPLICATION_NAME}-${env.POM_VERSION}.${env.POM_PACKAGING} ./.cicd
             ls -la ./.cicd
-            docker build --force-rm --no-cache --pull --rm=true --build-arg JAR_SOURCE=chathura-${env.APPLICATION_NAME}-${env.POM_VERSION}.${env.POM_PACKAGING} -t ${env.GOOGLE_ARTFICAT_REGISTRY}/${env.GOOGLE_DOCKER_REPO_NAME}/${env.APPLICATION_NAME}:${GIT_COMMIT}  ./.cicd
+            docker build --force-rm --no-cache --pull --rm=true --build-arg JAR_SOURCE=chathura-${env.APPLICATION_NAME}-${env.POM_VERSION}.${env.POM_PACKAGING} -t ${env.GOOGLE_ARTFICAT_REGISTRY}/${env.DEV_PROJECT_ID}/${env.GOOGLE_DOCKER_REPO_NAME}/${env.APPLICATION_NAME}:${GIT_COMMIT}  ./.cicd
             echo "***********Login to GAR Registry***********************"
             gcloud auth configure-docker ${env.GOOGLE_ARTFICAT_REGISTRY} --quiet
             echo "****************** Push Image to JFROG Registry ******************" 
